71b73650ca51f29aebabe7d73ca3ac014b369071
mohaninfy <65756236+mohaninfy@users.noreply.github.com>
2020-06-20 23:04:28 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 let User = require('../models/user.model');
 
 router.route('/').get((req,res) => {
-    debugger;
-    User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error :' + err));
+       res.json({message: 'success'})
+//     User.find()
+//     .then(users => res.json(users))
+//     .catch(err => res.status(400).json('Error :' + err));
 });
 
 router.route('/:id').get((req,res) => {
