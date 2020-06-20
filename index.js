be259fcf2c0d0e4845da12dc0f36fd896626e14b
mohaninfy <65756236+mohaninfy@users.noreply.github.com>
2020-06-20 22:44:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict"
 const express = require('express');
 const cors = require('cors');
 //const mongoose = require('mongoose');
@@ -5,11 +6,14 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.listen(port , () =>{
+    console.log(`Server running on port : ${port}`);
+});
 
 app.use(cors());
 app.use(express.json());
 // const uri = 'mongodb://mohanrockzz:Bornin%4091@cluster0-shard-00-00-du1sx.mongodb.net:27017,cluster0-shard-00-01-du1sx.mongodb.net:27017,cluster0-shard-00-02-du1sx.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
-const uri =  process.env.ATLAS_URI
+// const uri =  process.env.ATLAS_URI
 //mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 //mongoose.connection.once('open', ()=> {
 //     console.log('opened');
@@ -23,6 +27,3 @@ app.use('/register', registerRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 
-app.listen(port , () =>{
-    console.log(`Server running on port : ${port}`);
-});
